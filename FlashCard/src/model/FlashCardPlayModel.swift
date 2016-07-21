3165415504e656bbc5adfa0b45f4b2436ad3d689
Tasuku Tozawa <kamuhata.you@gmail.com>
2016-07-21 09:57:37 +0900

    Apply mvc to flash card play function

---
+++ FlashCard/src/model/FlashCardPlayModel.swift
@@ -0,0 +1,81 @@
+//
+//  FlashCardPlayModel.swift
+//  FlashCard
+//
+//  Created by tasuku tozawa on 2016/07/20.
+//  Copyright © 2016年 tasuwo. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+protocol FlashCardPlayModelDelegate {
+    func renderCardsText()
+}
+
+class FlashCardPlayModel {
+    var delegate: FlashCardPlayModelDelegate!
+    
+    private var cards = [(Int, String, String)]()
+    private var index = 0
+    private(set) var backCardText: String = ""
+    private(set) var frontCardText: String = ""
+    
+    init() {
+        // Load cards
+        let realm = try! Realm()
+        let cards = realm.objects(Card)
+        for card in cards {
+            self.cards.append((card.id, card.frontText, card.backText))
+        }
+        
+        // Shuffle cards
+        self.cards = self.cards.shuffle()
+        self.index = 0
+        self.backCardText = ""
+        
+        if self.cards.count > 0 {
+            self.frontCardText = self.cards[index].1
+        } else {
+            self.frontCardText = ""
+        }
+    }
+
+    func flipToNext() {
+        self.index += 1
+        
+        if self.cards.count == 0 { return }
+
+        if index >= self.cards.count {
+            self.index = self.cards.count
+            self.backCardText = self.cards[index-1].2
+            self.frontCardText = ""
+            
+            self.delegate.renderCardsText()
+            return
+        }
+        self.backCardText = self.cards[index-1].2
+        self.frontCardText = self.cards[index].1
+        
+        self.delegate.renderCardsText()
+    }
+
+    func flipToPrevious() {
+        self.index -= 1
+        
+        if self.cards.count == 0 { return }
+        
+        if self.index <= 0 {
+            self.index = 0
+            self.backCardText = ""
+            self.frontCardText = self.cards[index].1
+            
+            self.delegate.renderCardsText()
+            return
+        }
+        self.backCardText = self.cards[index-1].2
+        self.frontCardText = self.cards[index].1
+        
+        self.delegate.renderCardsText()
+    }
+}
