915d7d3b5f42ca08026a5ef91cba51b91fa7358f
Tasuku Tozawa <15nm722x@vc.ibaraki.ac.jp>
2016-09-07 00:27:36 +0900

    Coordinate vertical position of string in text field cell

--- FlashCard/src/model/common/NSCustomTextFieldCell.swift
+++ FlashCard/src/model/common/NSCustomTextFieldCell.swift
@@ -11,42 +11,9 @@ import Cocoa
 import Carbon
 
 class NSCustomTextFieldCell: NSTextFieldCell {
-
-    // 描画する長方形を返す
     override func drawingRectForBounds(theRect: NSRect) -> NSRect {
-        var rect: NSRect? = nil
-
-        if  let viewSize = self.controlView?.frame.size {
-            let stringSize = self.attributedStringValue.size()
-            rect = NSRect(
-                x: 1,
-                y: (viewSize.height - stringSize.height)/2-1,
-                width: viewSize.width-2,
-                height: viewSize.height-2
-            )
-        }
-
-        return rect==nil ? theRect : rect!
-    }
-    
-    
-    override func drawInteriorWithFrame(cellFrame: NSRect, inView controlView: NSView) {
-        NSColor.whiteColor().setFill()
-        NSRectFill(cellFrame)
-        super.drawInteriorWithFrame(self.titleRectForBounds(cellFrame), inView: controlView)
-    }
-
-
-    // タイトルテキストを描画する長方形を返す
-    override func titleRectForBounds(theRect: NSRect) -> NSRect {
-        var titleFrame = super.titleRectForBounds(theRect)
-        
-        let attrString = self.attributedStringValue
-        
-        titleFrame.origin.y = titleFrame.size.height - attrString.size().height
-        titleFrame.origin.y = titleFrame.size.height/2.0 - attrString.size().height/2.0
-        titleFrame.size.height = attrString.size().height*2
-        
-        return titleFrame
+        let textHeight: CGFloat = 40.0
+        let newRect = NSRect(x: 0, y: (theRect.size.height - textHeight) / 2, width: theRect.size.width, height: textHeight)
+        return super.drawingRectForBounds(newRect)
     }
 }
